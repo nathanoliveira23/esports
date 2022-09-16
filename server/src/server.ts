54cbfca43ed351d0c3cdbabc49e35f3916e7977a
nathanoliveira23 <nathanoliveira399@gmail.com>
2022-09-16 10:02:21 -0300

    create table game and ads

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({ message: "hello" });
+app.get('/games', (req, res) => {
+  return res.json([]);
 });
 
-app.listen(3333, () => "Server is running on PORT: 3333");
+app.post('/ads', (req, res) => {
+  return res.status(201).json([]);
+});
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.json();
+});
+
+app.listen(3333);
